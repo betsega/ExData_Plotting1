ed31cb8a189ef4461e3a8bcc7f3a98011d0a0c02
betsega <belaytsegak@gmail.com>
2019-09-07 12:51:53 -0500

    Initial commit to plot1.R

---
+++ plot1.R
@@ -0,0 +1,38 @@
+
+
+setwd("C://01 Mylocal driver/Coursera_Data_Science_Specialization/_Course4_Exploratory_DataAnalysis/Assignment1")
+
+
+
+### Read data file
+
+data<-read.table("household_power_consumption.txt", header=TRUE, sep=";",na.strings="?")
+
+
+## creat a variable of DateTime from Date and Time columns
+
+data$DateTime<-paste(data$Date, data$Time)
+
+## formate DateTime column using striptime
+
+data$DateTime<-strptime(data$DateTime, "%d/%m/%Y %H:%M:%S")
+
+### Extract data for the dates 2007-02-01 and 2007-02-02
+
+dat1<-which(data$DateTime==strptime("2007-02-01", "%Y-%m-%d"))
+
+dat2<-which(data$DateTime==strptime("2007-02-02 23:59:00", "%Y-%m-%d %H:%M:%S"))
+
+data_final<-data[start:end,]
+
+### Plot 1
+
+hist(as.numeric(as.character(data_final$Global_active_power)), # note we used as.character and as numeric since
+     # the variable is factor variable.
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)", col="red")
+
+
+dev.copy(png, "plot1.png", width=480, height=480)
+
+dev.off
